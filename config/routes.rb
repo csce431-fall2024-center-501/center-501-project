8f748cc1f9ea5f05903b1bf358a3c00b618be619
Kevin Johnson <john2003.kjj@gmail.com>
2024-09-20 21:09:37 -0500

    refined login/logout page and nav bar active selector, wrote more tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks:'users/omniauth_callbacks' }
   devise_scope :user do
     get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
-    get 'users/sign_out', to: 'users/sessions#destroy', as:
-    :destroy_user_session
+    get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
   end
   resources :users
   resources :educations
