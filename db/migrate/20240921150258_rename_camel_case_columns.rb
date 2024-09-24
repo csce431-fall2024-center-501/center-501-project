70ed01eb76e340e369d97f3d6768bd2ef706d3bf
Kevin Johnson <130250053+xkjjx@users.noreply.github.com>
2024-09-23 23:25:30 -0500

    Testing final sprint 1 changes (#13)
    * Education CRUD
    
    * RSPEC
    
    * Added comments
    
    * initial setup for home page (#4)
    
    basic navbar
    
    styling for navbar
    
    styling for navbar
    
    added placeholders for later data
    
    testing for home page
    
    testing on navbar
    
    added logo to navbar
    
    added logo to navbar
    
    added dummy data to navbar
    
    added dummy data to navbar
    
    Co-authored-by: Kevin Johnson <130250053+xkjjx@users.noreply.github.com>
    
    * Added authentication and basic user modal (#1)
    
    * added basic authentication flow
    
    * added automatic admin registration to certain users
    added user crud and admin verification for certain controller actions
    
    * Added step to set credentials for google oauth
    
    * refined code and tests
    
    * fixed admind vefrification at controller stage, added more tests for that
    
    * added comments
    frontend doesn't show user option to edit their own profule, consistent with controller
    
    * combined migrations, fixed show view to only show edit if admin
    
    * fixed authentication by using a different controller for authenticated routes
    
    * Create Procfile
    
    * Update main.yml to not run CI on every branch
    
    fixed typo in workflow file
    
    * Refinements to user experience (#6)
    
    * refined login/logout page and nav bar active selector, wrote more tests
    
    * fixed a lot of rubocop offenses
    
    * Google calendar (#8)
    
    * View Calendar Events
    
    * Add Event
    
    * Fix gitignore
    
    ---------
    
    Co-authored-by: Kevin Johnson <130250053+xkjjx@users.noreply.github.com>
    
    * merged credentials
    
    * Sponsorships (#7)
    
    * add CRUD
    
    * add links and change db column names
    
    * add rspec test cases
    
    * add donation link
    
    * removed sponsorship helpers and tests for it
    
    ---------
    
    Co-authored-by: Kevin Johnson <john2003.kjj@gmail.com>
    Co-authored-by: Kevin Johnson <130250053+xkjjx@users.noreply.github.com>
    
    * Added project pages and CRUD functionality. 98% coverage.
    
    * added links and tests for navbar
    
    * hotfix
    
    ---------
    
    Co-authored-by: ryanpavlik24 <ryan.pavlik@tamu.edu>
    Co-authored-by: ryanpavlik24 <112497973+ryanpavlik24@users.noreply.github.com>
    Co-authored-by: saradyl-tamu <122991011+saradyl-tamu@users.noreply.github.com>
    Co-authored-by: Pearlynn <98682935+PearlynnT@users.noreply.github.com>
    Co-authored-by: KailiF <kfogle6782@tamu.edu>
    Co-authored-by: KailiF <91387272+KailiF@users.noreply.github.com>
    Co-authored-by: saradyl-tamu <saradyl@tamu.edu>

---
+++ db/migrate/20240921150258_rename_camel_case_columns.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class RenameCamelCaseColumns < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :sponsorships, :sponsorName, :sponsor_name
+    rename_column :sponsorships, :sponsorLeadName, :sponsor_lead_name
+    rename_column :sponsorships, :sponsorPhone, :sponsor_phone
+    rename_column :sponsorships, :sponsorEmail, :sponsor_email
+    rename_column :sponsorships, :sponsorDonation, :sponsor_donation
+    rename_column :sponsorships, :sponsorEndOfContract, :sponsor_end_of_contract
+  end
+end
