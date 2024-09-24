c11a235afaf338cfa530008405ac5c4c98944b52
Kevin Johnson <130250053+xkjjx@users.noreply.github.com>
2024-09-23 22:39:26 -0500

    Revert "Merge Project Page to dev"

--- config/routes.rb
+++ config/routes.rb
@@ -15,12 +15,7 @@ Rails.application.routes.draw do
   end
   resources :users
   resources :educations
-  
-  resources :projects do
-    member do
-      get :delete
-    end
-  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
