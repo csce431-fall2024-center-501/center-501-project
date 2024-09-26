73fbeeacd1aaca49f81afa0e915f6f54b0791a3d
Kevin Johnson <130250053+xkjjx@users.noreply.github.com>
2024-09-25 20:23:02 -0500

    Improving and streamlining authentication (#17)
    * users don't have to login again when accessing calendars
    calendars is an authenticated route now
    
    * moved logic from view to controller for calendar
    > only calendars defined in constant EWB_CALENDAR_IDS now visible to user
    > attempting to create a new event or view an event for a non-EWB calendar is now forbidden

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-  get '/redirect', to: 'calendar#redirect', as: 'redirect'
-  get '/callback', to: 'calendar#callback', as: 'callback'
   get '/calendars', to: 'calendar#calendars', as: 'calendars'
   get '/events/:calendar_id', to: 'calendar#events', as: 'events', calendar_id: %r{[^/]+}
   post '/events/:calendar_id', to: 'calendar#new_event', as: 'new_event', calendar_id: %r{[^/]+}
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 2024_09_21_150258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_150258) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "projects", force: :cascade do |t|
     t.string "projectName"
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_150258) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "full_name"
@@ -53,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_21_150258) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
