0efd97cf211d1cdb06d12da194fcda5e618efefa
Kevin Johnson <john2003.kjj@gmail.com>
2024-09-25 20:17:12 -0500

    moved logic from view to controller for calendar > only calendars defined in constant EWB_CALENDAR_IDS now visible to user > attempting to create a new event or view an event for a non-EWB calendar is now forbidden

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,28 +1,42 @@
 class CalendarController < AuthenticatedApplicationController
+  EWB_CALENDAR_IDS = ['c_35657eacaf7df0315b9988c9b68e72be62b3d78e730edd6583459300f61320db@group.calendar.google.com',
+                        ]
+
   before_action :initialize_google_calendar_client
 
   def new_event
+    # Redirect and halt execution if calendar_id is not valid
+    unless EWB_CALENDAR_IDS.include?(params[:calendar_id])
+      redirect_to calendars_path and return
+    end
+  
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = @client
-
+  
     event = Google::Apis::CalendarV3::Event.new(
       summary: params[:event_name],
       start: Google::Apis::CalendarV3::EventDateTime.new(date: params[:start_date]),
       end: Google::Apis::CalendarV3::EventDateTime.new(date: params[:end_date])
     )
-
+  
     service.insert_event(params[:calendar_id], event)
     redirect_to events_url(calendar_id: params[:calendar_id])
   end
+  
 
   def calendars
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = @client
 
-    @calendar_list = service.list_calendar_lists
+    @calendar_list = service.list_calendar_lists.items.select { |calendar| EWB_CALENDAR_IDS.include?(calendar.id) }
   end
 
   def events
+    # TODO - add better redirect
+    unless EWB_CALENDAR_IDS.include?(params[:calendar_id])
+      redirect_to calendars_path and return
+    end
+
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = @client
 
