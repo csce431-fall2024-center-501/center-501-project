f1a6ce1d3bf2fe6c3e5153fb29dd760fbd5195b3
Kevin Johnson <john2003.kjj@gmail.com>
2024-09-29 15:49:16 -0500

    realized that going back to location user intended on going before login is not working removed broken code for now, maybe feature can be implemented later

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -30,13 +30,11 @@ module Users
     def after_omniauth_failure_path_for(_scope)
       new_user_session_path
     end
-
     def after_sign_in_path_for(resource_or_scope)
-      stored_location_for(resource_or_scope) || root_path
+      root_path
     end
 
     private
-
     def from_google_params
       @from_google_params ||= {
         uid: auth.uid,
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,13 +6,5 @@ module Users
     def after_sign_out_path_for(_resource_or_scope)
       new_user_session_path
     end
-
-    # after_sign_in_path_for is called when the user logs in
-    # basically, if the user was trying to access a page that required authentication,
-    # they will be redirected to that page after logging in
-    # otherwise, they will be redirected to the root path
-    def after_sign_in_path_for(resource_or_scope)
-      stored_location_for(resource_or_scope) || root_path
-    end
   end
 end
