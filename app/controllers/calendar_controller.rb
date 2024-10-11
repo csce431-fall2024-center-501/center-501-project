ee72262234c11acef4d7bf1155d109c95899b98e
ryanpavlik24 <112497973+ryanpavlik24@users.noreply.github.com>
2024-10-11 16:31:11 -0500

    Calendar function and style (#27)
    * Start of style
    
    * Overall Calendar Style
    
    * Calendar Style/Function & ReadMe
    
    * Calendar Style/Function & ReadMe
    
    * Calendar Style/Function & ReadMe
    
    * updated README
    
    * RSPEC Fixed for calendar
    
    ---------
    
    Co-authored-by: Kevin Johnson <john2003.kjj@gmail.com>

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -15,10 +15,48 @@ class CalendarController < AuthenticatedApplicationController
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = @client
 
+    start_date_time = begin
+      params[:start_datetime].to_datetime.rfc3339
+    rescue StandardError
+      nil
+    end
+    end_date_time = begin
+      params[:end_datetime].to_datetime.rfc3339
+    rescue StandardError
+      nil
+    end
+
+    start_date = begin
+      params[:start_date]
+    rescue StandardError
+      nil
+    end
+    end_date = begin
+      params[:end_date]
+    rescue StandardError
+      nil
+    end
+
     event = Google::Apis::CalendarV3::Event.new(
       summary: params[:event_name],
-      start: Google::Apis::CalendarV3::EventDateTime.new(date: params[:start_date]),
-      end: Google::Apis::CalendarV3::EventDateTime.new(date: params[:end_date])
+      start: if start_date_time
+               Google::Apis::CalendarV3::EventDateTime.new(
+                 date_time: (start_date_time.to_time + 5.hours).rfc3339
+               )
+             else
+               Google::Apis::CalendarV3::EventDateTime.new(
+                 date: start_date
+               )
+             end,
+      end: if end_date_time
+             Google::Apis::CalendarV3::EventDateTime.new(
+               date_time: (end_date_time.to_time + 5.hours).rfc3339
+             )
+           else
+             Google::Apis::CalendarV3::EventDateTime.new(
+               date: end_date
+             )
+           end
     )
 
     service.insert_event(params[:calendar_id], event)
@@ -30,10 +68,11 @@ class CalendarController < AuthenticatedApplicationController
     service.authorization = @client
 
     @calendar_list = service.list_calendar_lists.items.select { |calendar| EWB_CALENDAR_IDS.include?(calendar.id) }
+
+    redirect_to events_url(calendar_id: @calendar_list.first.id) if @calendar_list.length == 1
   end
 
   def events
-    # TODO: - add better redirect
     unless EWB_CALENDAR_IDS.include?(params[:calendar_id])
       flash[:alert] = 'Invalid calendar ID - only EWB calendars are supported.'
       redirect_to calendars_path and return
