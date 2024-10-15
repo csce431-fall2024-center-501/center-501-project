8c905a5119f31de69d4d61a51ee2df07669140c0
ryanpavlik24 <ryan.pavlik@tamu.edu>
2024-10-15 03:37:45 +0000

    Fix

--- app/mailers/previews/admin_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-class AdminMailerPreview < ActionMailer::Preview
-  def welcome_email
-    user1 = User.new(name: 'John Doe', email: 'joe@gmail.com')
-    AdminMailer.with(user: user1).welcome_email
-  end
-end
