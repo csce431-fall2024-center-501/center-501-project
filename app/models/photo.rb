e7246de0912cd98e252be7163be1db43301c07c5
Pearlynn <98682935+PearlynnT@users.noreply.github.com>
2024-10-30 12:05:45 -0500

    Add validation check for input fields

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,6 @@
 class Photo < ApplicationRecord
     TYPES = %w[.jpg .png].freeze
+    validates :photoDescription, presence: true
+    validates :photoType, presence: true
+    validates :photoDescription, presence: true
 end
