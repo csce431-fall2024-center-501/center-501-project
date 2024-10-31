96ab38004740cce17e8052ca8f7ac7bb086b79a7
ryanpavlik24 <112497973+ryanpavlik24@users.noreply.github.com>
2024-10-31 11:01:21 -0500

    Email various types (#63)
    * Start of new emails
    
    * New Emails Added
    
    * Email Fixed
    
    * Emails Fixed // Weekly Email Temp Added

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,29 @@
 class AdminMailer < ApplicationMailer
-  def blast_email(recipients, subject, message)
+  def indiviual_email(recipients, subject, message)
     @message = message
     mail(to: recipients, subject:)
+  end
+
+  def active_member_email(subject, message)
+    @message = message
+    @users = User.where('grad_date > ?', Date.today)
+    mail(to: @users.pluck(:email), subject:) if @users.present?
+  end
+
+  def active_inactive_member_email(subject, message)
+    @message = message
+    @users = User.all
+    mail(to: @users.pluck(:email), subject:) if @users.present?
+  end
+
+  def birthday_email(person)
+    @person = person
+    mail(to: @person.pluck(:email), subject: "Happy Birthday#{person.full_name}!") if @users.present?
+  end
+
+  def calendar_email(subject, event_list)
+    @users = User.where('grad_date > ?', Date.today)
+    @event_list = event_list
+    mail(to: @users.pluck(:email), subject:) if @users.present?
   end
 end
