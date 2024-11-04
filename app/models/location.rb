e7018eb9a7f6cc944b100c1dba101fb03102563b
Pearlynn <98682935+PearlynnT@users.noreply.github.com>
2024-11-04 02:48:13 -0600

    Add input check for location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+    validates :address, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip_code, presence: true, numericality: { greater_than_or_equal_to: 0 }
+    validates :country, presence: true
 end
