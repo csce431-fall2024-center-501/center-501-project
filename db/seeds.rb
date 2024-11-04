c7bfb19cb888797ff93d46db2a6c572eefe25eee
Kevin Johnson <john2003.kjj@gmail.com>
2024-11-04 17:30:30 -0600

    added sponsorship logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -155,7 +155,6 @@ MARKDOWN
   }
 ])
 
-# Creating User and Sponsorship records as in the original code
 100.times do
   User.create!(
     email: Faker::Internet.unique.email,
@@ -183,7 +182,8 @@ Sponsorship.create!([
     sponsor_email: "test@gmail.com",
     sponsor_donation: 500,
     sponsor_end_of_contract: 1.year.from_now.to_date,
-  },
+    sponsor_logo: "https://res.cloudinary.com/daw03xxlr/image/upload/v1730658925/btgi2wlslxgb30fnt7p0.png"
+  }, 
   {
     sponsor_name: "Texas A&M Student Engineers' Council",
     sponsor_lead_name: "sec_lead",
@@ -191,27 +191,9 @@ Sponsorship.create!([
     sponsor_email: "test@gmail.com",
     sponsor_donation: 1000,
     sponsor_end_of_contract: 1.year.from_now.to_date,
+    sponsor_logo: "https://res.cloudinary.com/daw03xxlr/image/upload/v1730660069/blyrbwrnxobl8nk5qa3p.png"
   }
 ])
-
-100.times do
-    User.create!(
-        email: Faker::Internet.unique.email,
-        full_name: Faker::Name.name,
-        phone_number: Faker::PhoneNumber.unique.subscriber_number(length: 10), # Phone number
-        ring_date: Faker::Date.between(from: '1990-01-01', to: Date.today),   # Date within range
-        grad_date: Faker::Date.between(from: '1990-01-01', to: Date.today),   # Date within range
-        birthday: Faker::Date.between(from: '1990-01-01', to: '2000-01-01'),                  # Birthday within range
-        uid: Faker::Number.unique.number(digits: 10),
-        avatar_url: Faker::Avatar.image,
-        user_type: ['user', 'admin'].sample,                                                # Randomly assigns either 'user' or 'admin'
-        class_year: Faker::Number.between(from: 1900, to: Date.today.year + 4),             # Random class year
-        shirt_size: ['S', 'M', 'L', 'XL'].sample,                                           # Random shirt size
-        dietary_restriction: ['None', 'Vegetarian', 'Vegan', 'Gluten-Free', 'Halal'].sample, # Random dietary restriction
-        account_complete: true,
-        linkedin_url: Faker::Internet.url
-    )
-end
 
 Location.create!(
   address: "400 Bizzell St",
